e3d779cbf281cac48e81deac5907b3d7b43f3d07
qnblackcat <qnblackcat@gmail.com>
2022-05-04 11:10:19 +0700

    code clean up

--- buildapp.sh
+++ buildapp.sh
@@ -4,26 +4,30 @@
 cd "$(dirname "$0")"	
 	
 # Check uYou
-	if [[ ! -f Tweaks/uYou/com.miro.uyou_2.1_iphoneos-arm.deb ]]
+if 	[ ! -f Tweaks/uYou/com.miro.uyou_2.1_iphoneos-arm.deb ]
 then
-    	echo -e "==> \033[1muYou v2.1 is not found. Downloading uYou (v2.1)...\033[0m"
+	echo -e "==> \033[1muYou v2.1 is not found. Downloading uYou (v2.1)...\033[0m"
 	(set -x ; curl https://miro92.com/repo/debs/com.miro.uyou_2.1_iphoneos-arm.deb --output Tweaks/uYou/com.miro.uyou_2.1_iphoneos-arm.deb)
 else
-    	echo -e "==> \033[1mFounded uYou (v2.1)!\033[0m"
-	fi
+	echo -e "==> \033[1mFounded uYou (v2.1)!\033[0m"
+fi
 
 # Extract uYou
 	echo -e "==> \033[1mExtracting uYou...\033[0m"
-	if (cd Tweaks/uYou && tar -xf com.miro.uyou_2.1_iphoneos-arm.deb && tar -xf data.tar.*)
+if 	(cd Tweaks/uYou && tar -xf com.miro.uyou_2.1_iphoneos-arm.deb && tar -xf data.tar.*)
 then
 	echo -e "\033[1m> Extracted uYou!\033[0m"
 else
 	echo "> \033[1mCouldn't extract uYou\033[0m"
-	fi
+fi
 
 # Makefile
+if 	[ -d ./tmp ]
+then
+	rm -rf ./tmp
+fi
 	read -e -p "==> Path to the decrypted YouTube.ipa or YouTube.app: " PATHTOYT
-if [[ $PATHTOYT == *.ipa ]]
+if 	[[ $PATHTOYT == *.ipa ]]
 then 
 	unzip -q "$PATHTOYT" -d ./tmp
 	rm -rf ./tmp/Payload/YouTube.app/PlugIns/*.appex
@@ -31,7 +35,7 @@ then
 	make package
 	open ./packages
 
-elif [[ $PATHTOYT == *.app ]]
+elif 	[[ $PATHTOYT == *.app ]]
 then
 	mkdir -p ./tmp/Payload/
 	cp -R "$PATHTOYT" ./tmp/Payload 2>/dev/null
