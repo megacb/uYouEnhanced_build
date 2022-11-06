6b3026159ca03b741978e2a695cdf5a06bc84aa0
qnblackcat <qnblackcat@gmail.com>
2022-11-06 11:22:31 +0700

    clean up

--- buildapp.sh
+++ buildapp.sh
@@ -22,27 +22,29 @@ else
 fi
 
 # Makefile
-if 	[ -d ./tmp ]
+if 	[ -d tmp ]
 then
-	rm -rf ./tmp
+	rm -rf tmp
 fi
 	read -e -p "==> Path to the decrypted YouTube.ipa or YouTube.app: " PATHTOYT
 if 	[[ $PATHTOYT == *.ipa ]]
 then 
-	unzip -q "$PATHTOYT" -d ./tmp
-	rm -rf ./tmp/Payload/YouTube.app/PlugIns/*.appex
+	unzip -q "$PATHTOYT" -d tmp
+	rm -rf tmp/Payload/YouTube.app/_CodeSignature/CodeResources
+	rm -rf tmp/Payload/YouTube.app/PlugIns/*.appex
 	cp -R Extensions/*.appex tmp/Payload/YouTube.app/PlugIns 
-	make package
-	open ./packages
+	make package FINALPACKAGE=1
+	open packages
 
 elif 	[[ $PATHTOYT == *.app ]]
 then
-	mkdir -p ./tmp/Payload/
-	cp -R "$PATHTOYT" ./tmp/Payload 2>/dev/null
-	rm -rf ./tmp/Payload/YouTube.app/PlugIns/*.appex
+	mkdir -p tmp/Payload/
+	cp -R "$PATHTOYT" tmp/Payload 2>/dev/null
+	rm -rf tmp/Payload/YouTube.app/_CodeSignature/CodeResources
+	rm -rf tmp/Payload/YouTube.app/PlugIns/*.appex
 	cp -R Extensions/*.appex tmp/Payload/YouTube.app/PlugIns 
-	make package
-	open ./packages
+	make package FINALPACKAGE=1
+	open packages
 else
 	echo "This is not an ipa/app!"
 fi
@@ -50,3 +52,4 @@ fi
 	tput setaf 1 && echo -e "==> \033[1mCleaning up...\033[0m"
 	find Tweaks/uYou -mindepth 1 ! -name "com.miro.uyou_2.1_iphoneos-arm.deb" ! -name ".gitkeep" -exec rm -rf {} \; 2>/dev/null
 	rm -rf tmp/ Resources .theos/_/Payload
+	echo -e  "==> \033[1mSHASUM256: $(shasum -a 256 packages/*.ipa)\033[0m"	
