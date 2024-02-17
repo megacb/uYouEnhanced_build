0944e52aeff1fbac14dccb086468aa66ba5e85ed
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-17 02:18:50 -0600

    Create RootOptionsController.h

---
+++ Sources/RootOptionsController.h
@@ -0,0 +1,5 @@
+#import "UIKit/UIKit.h"
+
+@interface RootOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView* tableView;
+@end
