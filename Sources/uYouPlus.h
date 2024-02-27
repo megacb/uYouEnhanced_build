1a1273055f17612d805054379eb53bc99d575f4a
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-27 17:48:45 -0600

    Update uYouPlus.h

--- Sources/uYouPlus.h
+++ Sources/uYouPlus.h
@@ -175,19 +175,3 @@
 
 @interface YTPivotBarIndicatorView : UIView
 @end
-
-// Snack bar
-@interface YTHUDMessage : NSObject
-+ (id)messageWithText:(id)text;
-- (void)setAction:(id)action;
-@end
-
-@interface GOOHUDMessageAction : NSObject
-- (void)setTitle:(NSString *)title;
-- (void)setHandler:(void (^)(id))handler;
-@end
-
-@interface GOOHUDManagerInternal : NSObject
-- (void)showMessageMainThread:(id)message;
-+ (id)sharedInstance;
-@end
