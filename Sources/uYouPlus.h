e8a16db8c51e8b8ff936f3b5570f6c7a3989c15a
Bryce Hackel <34104885+bhackel@users.noreply.github.com>
2024-03-17 23:00:12 -0700

    Update YoutubeHeader submodule

--- Sources/uYouPlus.h
+++ Sources/uYouPlus.h
@@ -52,14 +52,6 @@
 @end
 
 // YTTapToSeek - https://github.com/bhackel/YTTapToSeek
-@interface YTInlinePlayerBarContainerView : UIView
-@property (nonatomic, weak, readwrite) id delegate;
-- (void)didPressScrubber:(id)arg1;
-- (CGFloat)scrubRangeForScrubX:(CGFloat)arg1;
-@end
-@interface YTMainAppVideoPlayerOverlayViewController : UIViewController
-- (CGFloat)totalTime;
-@end
 @interface YTPlayerViewController : UIViewController
 - (void)seekToTime:(double)time;
 @end
