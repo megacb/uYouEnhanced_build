31e8568c4d24674d6ee3a560b035f3a46c539e6a
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-04-15 22:20:25 -0500

    Replace UIAlertView with YTAlertView

--- Sources/AppIconOptionsController.m
+++ Sources/AppIconOptionsController.m
@@ -4,10 +4,8 @@
 @interface AppIconOptionsController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (strong, nonatomic) UITableView *tableView;
-@property (strong, nonatomic) UIImageView *iconPreview;
 @property (strong, nonatomic) NSArray<NSString *> *appIcons;
 @property (assign, nonatomic) NSInteger selectedIconIndex;
-@property (assign, nonatomic) NSInteger defaultIconIndex;
 
 @end
 
@@ -23,7 +21,7 @@
     [titleLabel sizeToFit];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:titleLabel];
 
-    self.selectedIconIndex = 0;
+    self.selectedIconIndex = -1;
     
     self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
     self.tableView.dataSource = self;
@@ -42,11 +40,6 @@
     
     self.navigationItem.rightBarButtonItems = @[resetButton, saveButton];
     
-    self.iconPreview = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.bounds.size.width - 80, self.view.bounds.size.height - 80, 60, 60)];
-    self.iconPreview.layer.cornerRadius = 10.0;
-    self.iconPreview.clipsToBounds = YES;
-    [self.view addSubview:self.iconPreview];
-    
     NSString *path = [[NSBundle mainBundle] pathForResource:@"uYouPlus" ofType:@"bundle"];
     NSBundle *bundle = [NSBundle bundleWithPath:path];
     self.appIcons = [bundle pathsForResourcesOfType:@"png" inDirectory:@"AppIcons"];
@@ -56,11 +49,7 @@
     }
 }
 
-- (NSArray<NSString *> *)sortedAppIcons {
-    return [self.appIcons sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger section {
     return self.appIcons.count;
 }
 
@@ -73,12 +62,12 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
     }
-    [self sortedAppIcons];
+    
     NSString *iconPath = self.appIcons[indexPath.row];
     cell.textLabel.text = [iconPath.lastPathComponent stringByDeletingPathExtension];
     
     UIImage *iconImage = [UIImage imageWithContentsOfFile:iconPath];
-    cell.imageView.image = [self resizedImageWithImage:iconImage];
+    cell.imageView.image = iconImage;
     cell.imageView.layer.cornerRadius = 10.0;
     cell.imageView.clipsToBounds = YES;
         
@@ -94,16 +83,8 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    if (indexPath.row == 0) {
-        self.selectedIconIndex = -1;
-        [self.tableView reloadData];
-        self.iconPreview.image = nil;
-    } else {
-        self.defaultIconIndex = 0;
-        NSString *selectedIconPath = self.appIcons[indexPath.row - 1];
-        UIImage *selectedIconImage = [UIImage imageWithContentsOfFile:selectedIconPath];
-        self.iconPreview.image = [self resizedImageWithImage:selectedIconImage];
-    }
+    self.selectedIconIndex = indexPath.row;
+    [self.tableView reloadData];
 }
 
 - (void)resetIcon {
@@ -115,18 +96,12 @@
             NSLog(@"Icon reset successfully");
             [self showAlertWithTitle:@"Success" message:@"Icon reset successfully"];
             [self.tableView reloadData];
-            self.iconPreview.image = nil;
         }
     }];
 }
 
 - (void)saveIcon {
-    NSString *selectedIconPath;
-    if (self.selectedIconIndex == -1) {
-        selectedIconPath = nil;
-    } else {
-        selectedIconPath = self.appIcons[self.selectedIconIndex];
-    }
+    NSString *selectedIconPath = self.selectedIconIndex >= 0 ? self.appIcons[self.selectedIconIndex] : nil;
     
     [[UIApplication sharedApplication] setAlternateIconName:selectedIconPath completionHandler:^(NSError * _Nullable error) {
         if (error) {
@@ -139,22 +114,12 @@
     }];
 }
 
-- (UIImage *)resizedImageWithImage:(UIImage *)image {
-    CGFloat scale = [UIScreen mainScreen].scale;
-    CGSize newSize = CGSizeMake(image.size.width / scale, image.size.height / scale);
-    UIGraphicsBeginImageContextWithOptions(newSize, NO, scale);
-    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return resizedImage;
-}
-
 - (void)showAlertWithTitle:(NSString *)title message:(NSString *)message {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
-        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
-        [alert addAction:okAction];
-        [self presentViewController:alert animated:YES completion:nil];
+        YTAlertView *alert = [[YTAlertView alloc] init];
+        alert.title = title;
+        alert.subtitle = message;
+        [alert show];
     });
 }
 
