63ae617fbd9fae46d0eb01b99df2462933749637
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-04-15 23:50:27 -0500

    Update AppIconOptionsController.h

--- Sources/AppIconOptionsController.h
+++ Sources/AppIconOptionsController.h
@@ -1,20 +1,5 @@
 #import <UIKit/UIKit.h>
 
-// YTAlertView
-@interface YTAlertView : UIView
-@property (nonatomic, copy, readwrite) UIImage *icon;
-@property (nonatomic, copy, readwrite) NSString *title;
-@property (nonatomic, copy, readwrite) NSString *subtitle;
-@property (nonatomic, strong, readwrite) UIView *customContentView;
-@property (nonatomic, assign, readwrite) UIEdgeInsets customContentViewInsets; //(8, 24, 0, 24) by default
-- (CGRect)frameForDialog;
-- (void)show;
-
-+ (instancetype)infoDialog;
-+ (instancetype)confirmationDialogWithAction:(void (^)(void))action actionTitle:(NSString *)actionTitle;
-@end
-
-// AppIconOptionsController
 @interface AppIconOptionsController : UIViewController
 
 @end
