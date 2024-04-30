7bf6294385094badcf5276ebb49e5c63380e46e0
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-04-29 20:34:03 -0500

    Update AppIconOptionsController.h

--- Sources/AppIconOptionsController.h
+++ Sources/AppIconOptionsController.h
@@ -2,4 +2,21 @@
 
 @interface AppIconOptionsController : UIViewController
 
+@property (strong, nonatomic) UIButton *backButton;
+
+@end
+
+@interface UIImage (CustomImages)
+
++ (UIImage *)customBackButtonImage;
+
+@end
+
+@implementation UIImage (CustomImages)
+
++ (UIImage *)customBackButtonImage {
+    NSBundle *bundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"uYouPlus" ofType:@"bundle"]];
+    return [UIImage imageNamed:@"Back.png" inBundle:bundle compatibleWithTraitCollection:nil];
+}
+
 @end
